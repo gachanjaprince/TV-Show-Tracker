a7726f256167c536510c7e7962f0da40d6b511c6
gachanjaprince <gachanjaprince@gmail.com>
2024-03-24 13:13:10 -0600

    Added featured shows carousel

--- public/js/main.js
+++ public/js/main.js
@@ -47,3 +47,29 @@ function searchShows(){
             }
         });
 }
+
+// SLIDE CAROUSEL
+let slideIndex =1;
+showSlides (slideIndex)
+
+//Next/Previous controls
+function plusSlides (n) {
+    showSlides (slideIndex += n);
+}
+
+function currentSlide (n) {
+    showSlides (slideIndex = n);
+}
+
+
+function showSlides (n) {
+    let i;
+    let slides = document.getElementsByClassName("my-slides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+
+    slides [slideIndex-1].style.display = "block";
+}
